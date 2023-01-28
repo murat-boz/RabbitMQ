bb00d95209e8eb45b7a4ae8c5e292dfcd4edcad5
murat-boz <muratboz@hotmail.com>
2023-01-29 00:20:22 +0300

    Add project files.

---
+++ src/HelloWorld/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://xzrqekfn:b9uoZ3yYdMp7phrIEE4yPFN9Nr6q5dZF@moose.rmq.cloudamqp.com/xzrqekfn");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                channel.QueueDeclare("hello-queue", true, false, false);
+
+                string message = "hello world";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine("Message has been sent.");
+            }
+        }
+    }
+}
---
+++ src/HelloWorld/RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://xzrqekfn:b9uoZ3yYdMp7phrIEE4yPFN9Nr6q5dZF@moose.rmq.cloudamqp.com/xzrqekfn");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                channel.QueueDeclare("hello-queue", true, false, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                channel.BasicConsume("hello-queue", true, consumer);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                    Console.WriteLine($"The message {message}");
+                };
+
+                Console.WriteLine("Message has been received.");
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
